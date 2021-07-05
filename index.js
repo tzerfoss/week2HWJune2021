e56b7fff6bea215ea198b2a9396f54d63ce78d4e
tzerfoss <tandizerfoss@gmail.com>
2021-07-05 18:47:42 -0500

    commit -m saved changes

---
+++ index.js
@@ -0,0 +1,16 @@
+let position = 0;
+        
+const nextSlide = function () { 
+            const imageSlider = document.getElementById('imageSlider');
+            imageSlider.style.transform = 'translateX()';
+        } 
+        
+let position = 0;
+        const nextSlide = function () { 
+            const imageSlider = document.getElementById('imageSlider2');
+            imageSlider.style.transform = 'translateX()';
+        } 
+const myFunction = function () {
+          document.getElementById('myHeader').style.backgroundColor = 'red';
+        }
+     
